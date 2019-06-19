abf4a7965341c84a7f8bad19a35716999011cfbf
hliblack <hliblack@outlook.com>
2019-06-19 17:17:44 +0800

    v1.3

--- roles/ubuntu_zabbix/files/init-57.sh
+++ roles/ubuntu_zabbix/files/init-57.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-old_password=$(cat /credentials/password.txt | awk -F ":" '{print $2}' )
+old_password=$(cat /credentials/password.txt | awk -F ':' '{print $2}' | sed -n '2p' )
 root_password=$(</dev/urandom tr -dc '0-9!@#$%a-zA-Z' | head -c10)
 zabbix_password=$(</dev/urandom tr -dc '0-9!@#$%a-zA-Z' | head -c10)
 
 systemctl restart mysqld
 mysqladmin -uroot -p${old_password} -h localhost password $root_password
-mysqladmin -uzabbix -p${old_password} -h localhost password $zabbix_password
 
-echo 'Databases username: root  Databases password: '$root_password  > /credentials/password.txt
-echo 'Databases username: pimcore  Databases password: '$zabbix_password  >> /credentials/password.txt
+# 修改mariadb普通用户密码
+touch /root/changepass.txt
+echo -e "use mysql;\nupdate user set password = password('$zabbix_password') where user = 'zabbix';\nflush privileges;" > /root/changepass.txt
+mysql -uroot -p${root_password} < /root/changepass.txt
 
+echo -e 'Database username: root \nDatabase password:'$root_password  > /credentials/password.txt
+echo -e 'Database username: zabbix \nDatabase password:'$zabbix_password  >> /credentials/password.txt
+
+sed -i "s/DBPassword=${old_password}/DBPassword=${zabbix_password}/g" /etc/zabbix/zabbix_server.conf
+systemctl restart zabbix-server
 
 sed -i "s/\/root\/init.sh//" /etc/rc.local
-rm -rf /root/init.sh
-
+rm -rf /root/init.sh /root/changepass.txt
