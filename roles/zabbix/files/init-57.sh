1b2eef30a5f38a6cb3907a1cc65f58784a9156a5
hliblack <hliblack@163.com>
2019-11-20 17:57:55 +0800

    password

--- roles/zabbix/files/init-57.sh
+++ roles/zabbix/files/init-57.sh
@@ -14,12 +14,12 @@ mysql -uroot -p${old_password} < /root/changepass.txt
 # 修改root密码
 mysqladmin -uroot -p${old_password} password ${new_root_password}
 
-sed -i "s/DBPassword=${old_password}/DBPassword=${zabbix_password}/g" /etc/zabbix/zabbix_server.conf
-sed -i "s/DBPassword=${old_password}/DBPassword=${zabbix_password}/g" /etc/zabbix/web/zabbix.conf.php
-systemctl restart zabbix-server
-
 sed -i "s/MySQL password:${old_password}/MySQL password:${new_root_password}/g" /credentials/password.txt
 sed -i "s/database password:${old_password}/database password:${zabbix_password}/g" /credentials/password.txt
+
+sed -i "s/DBPassword=${old_password}/DBPassword=${zabbix_password}/g" /etc/zabbix/zabbix_server.conf
+sed -i "s/${old_password}/${zabbix_password}/g" /etc/zabbix/web/zabbix.conf.php
+systemctl restart zabbix-server
 
 sed -i "s/\/root\/init.sh//" /etc/rc.local
 rm -rf /root/init.sh /root/changepass.txt
