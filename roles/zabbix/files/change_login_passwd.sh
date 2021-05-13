bff5f7d0c56e654a78edce755de9ea2fc21541b7
zengxc <zxc916684@163.com>
2021-05-13 15:48:43 +0800

    update init

--- roles/zabbix/files/change_login_passwd.sh
+++ roles/zabbix/files/change_login_passwd.sh
@@ -1,8 +1,8 @@
 field_lines=`mysql -uroot -p$new_password -h 127.0.0.1 -NBe "select count(*) from zabbix.users where  alias='Admin';"`
 while [ $field_lines -ge 1 ]
 do
-    password_lines=`mysql -uroot -p$new_password -h 127.0.0.1 -NBe "select count(*) from zabbix.users where  length(passwd) = 60;"`
-    if [ $password_lines -ge 2 ];then
+    password_lines=`mysql -uroot -p$new_password -h 127.0.0.1 -NBe "select count(*) from zabbix.users where  length(passwd) < 60;"`
+    if [ $password_lines -ne 0 ];then
         sudo mysql -uroot -p"$new_password" -h 127.0.0.1 -e "update zabbix.users set passwd=md5('$new_password') where alias='Admin';"
     else
     break
